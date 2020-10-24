60ff377b310dfaf2ebf4fb99b4e807dbea474b58
shan2812 <shanicereid406@gmail.com>
2020-10-24 00:34:16 -0500

    Edited starter files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-/* Add your JavaScript to this file */
+/*function validate(){
+    var empt= document.forms[''][''].value;
+    if (empt==''){
+        alert();
+        return false;
+    }
+    alert('Please Enter a Valid Email Address.')
+    return true;*/
+
+    var submit = document.getElementsByClassName('message');
+    document.getElementById('')
